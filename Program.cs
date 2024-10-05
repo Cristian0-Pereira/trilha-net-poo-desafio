f539e9612e9e71ceee269f192f13c4f81c982d62
Cristian0-Pereira <cristianops2020@gmail.com>
2024-10-05 01:32:35 -0300

    Desafio Dio POO
    Finalizado!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
-﻿using DesafioPOO.Models;
+﻿using System.ComponentModel;
+using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+// NOKIA
+Console.WriteLine("Smartphone Nokia:");
+Nokia meuNokia = new("123456", "Modelo 1", "123456",  235);
+meuNokia.Ligar();
+meuNokia.InstalarAplicativo("\"Whatsapp\"");
+
+// IPHONE
+Console.WriteLine("Smartphone iPhone:");
+Iphone meuIphone = new("543211", "Modelo 2", "9992929", 556);
+meuIphone.ReceberLigacao();
+meuIphone.InstalarAplicativo("\"Telegram\"");
